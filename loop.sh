0acb221ff934ee1eaa26dcd498388a42bea68f25
Maxence Brette <maxbrette@gmail.com>
2018-02-10 15:59:56 -0600

    first commit

---
+++ loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function helloworld() {
+ echo "helloworld"
+}
+
+for i in 1 2 3 4 5
+do
+#mkdir "folder"$i
+#touch "folder"$i/emptyfile.txt
+#rmdir "folder"$i
+#echo "hello" >> "folder"$i/newfile.txt
+helloworld
+done
